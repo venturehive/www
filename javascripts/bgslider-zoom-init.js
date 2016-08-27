f28422937fc10ef843747a5004408ae66f801824
Luis Amat <luis@venturehive.com>
2016-08-27 12:11:38 -0400

    first commit since moving from flitepath repo

---
+++ javascripts/bgslider-zoom-init.js
@@ -0,0 +1,58 @@
+/*global $:false */
+/*global window: false */
+
+(function(){
+  "use strict";
+
+
+$(function ($) {
+
+    //BG SLIDESHOW WITH ZOOM EFFECT
+    $.mbBgndGallery.buildGallery({
+                containment:"body",
+                timer:1000,
+                effTimer:15000,
+                controls:false, //updated in v1.1
+                grayScale:false,
+                shuffle:true,
+                preserveWidth:false,
+                preserveTop: true,
+                effect:"zoom",
+	//effect:{enter:{transform:"scale("+(1+ Math.random()*2)+")",opacity:0},exit:{transform:"scale("+(Math.random()*2)+")",opacity:0}},
+
+                // If your server allow directory listing you can use:
+                // (however this doesn't work locally on your computer)
+
+                //folderPath:"testImage/",
+
+                // else:
+
+                 images:[
+                 "images/bg/08.jpg",
+                 "images/bg/09.jpg",
+                 ],
+
+                onStart:function(){},
+                onPause:function(){},
+                onPlay:function(opt){},
+                onChange:function(opt,idx){},
+                onNext:function(opt){},
+                onPrev:function(opt){}
+            });
+
+
+   
+});
+// $(function ($)  : ends
+
+})();
+//  JSHint wrapper $(function ($)  : ends
+
+
+
+
+
+
+
+	
+
